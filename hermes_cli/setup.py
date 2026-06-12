d18e5f34a7c59312ed7ac1898e562611b788dedc
Wesley Simplicio <wesleysimplicio@live.com>
2026-06-12 16:49:12 -0300

    chore(hermes/cli): ruff auto-fix unused imports and variables

--- hermes_cli/setup.py
+++ hermes_cli/setup.py
@@ -23,7 +23,6 @@ from typing import Optional, Dict, Any
 
 from hermes_cli.nous_subscription import get_nous_subscription_features
 from tools.tool_backend_helpers import managed_nous_tools_enabled
-from utils import base_url_hostname
 from hermes_constants import get_optional_skills_dir
 
 logger = logging.getLogger(__name__)
